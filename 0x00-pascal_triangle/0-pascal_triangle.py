c1b1b0fe19f4a37a31bcedc1324a11e8ffe2327c
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-27 14:27:53 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,1 @@
-#!/usr/bin/python3 
 
-def pascal_triangle(n):
-    """ that returns a list of lists of integers representing the Pascal’s triangle of n """
-    if type(n) != int or n < 0:
-        raise ValueError("Input must be nonnegative integer.")
-    result = []
-    for i in range(n+1):                   
-        result.append([])             
-        result[i].extend([1]*(i+1))        
-        if i > 0:                                                    
-            for j in range(len(result[i-1])):       
-                result[i][j] += result[i-1][j]              
-    return result
-print(pascal_triangle(4))
