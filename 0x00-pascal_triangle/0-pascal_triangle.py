5a229f5c8d7601e3a7191bec2a3dabf3b9484fcd
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-27 14:26:23 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3 
-# Pascal Triangle -> alx interview  question
 
 def pascal_triangle(n):
     """ that returns a list of lists of integers representing the Pascal’s triangle of n """
-
     if type(n) != int or n < 0:
         raise ValueError("Input must be nonnegative integer.")
     result = []
-    for i in range(n+1):                   # Loop through each line (row).
-        result.append([])                   # Start new sublist for this line.
-        result[i].extend([1]*(i+1))         # Fill with ones, then zeros.
-        if i > 0:                                                     # Not on first line?
-            for j in range(len(result[i-1])):       # Copy previous line's values.
-                result[i][j] += result[i-1][j]               # Add next value from prev.
+    for i in range(n+1):                   
+        result.append([])             
+        result[i].extend([1]*(i+1))        
+        if i > 0:                                                    
+            for j in range(len(result[i-1])):       
+                result[i][j] += result[i-1][j]              
     return result
-
 print(pascal_triangle(4))
