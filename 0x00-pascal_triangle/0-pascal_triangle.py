c796bfc72c1eb1c9c5ad99ecd9edb63db3218ab9
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-27 14:34:06 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,16 +2,20 @@
 """
 Pascal triangle -> alx interview 
 """
+
+
 def pascal_triangle(n):
     """Pass in a number n and return a list of lists representing the first n rows
     of Pascal's Triangle."""
     triangle = []
     if n > 0:
         triangle = [[1]]
+
         for i in range(1, n):
             row = [1]
             for j in range(1, i):
                 row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
             row.append(1)
             triangle.append(row)
+
     return triangle
