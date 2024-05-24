71d77dd6e4dd74b1fd519c7667c72aa562d79de0
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-05-24 01:05:27 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python3
 
-""" UTF-8 Validation """
+""" Utf-8 validation"""
 
 
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
-    # Masks to check the leading bits in a byte
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
+    check1 = 1 << 7
+    check2 = 1 << 6
 
-    # Iterate through each integer in the data
     for byte in data:
-        mask = 1 << 7
+        check1 = 1 << 7
 
         if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
-            while mask & byte:
+            while check1 & byte:
                 num_bytes += 1
-                mask >>= 1
-
-            # 1 byte character or invalid leading byte
+                check1 >>= 1
             if num_bytes == 0:
                 continue
-
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            # Check if the byte is a valid continuation byte
-            if not (byte & mask1 and not (byte & mask2)):
+            if not (byte & check1 and not (byte & check2)):
                 return False
-
         num_bytes -= 1
 
-    # All characters should have been fully parsed
     return num_bytes == 0
