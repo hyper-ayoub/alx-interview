d7b19364348002183c5652371c156976db159cd1
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-07-04 15:10:26 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,41 +1,44 @@
 #!/usr/bin/python3
-
-""" solution """
+"""
+This script solves the N-Queens problem for a given N.
+Usage: nqueens N
+N must be an integer greater or equal to 4.
+"""
 
 import sys
 
-    def solveNQueens(self, n: int) -> List[List[str]]:
-        col = set()
-        posDiag = set()  # (r + c)
-        negDiag = set()  # (r - c)
+def solve_nqueens(n):
+    col = set()
+    posDiag = set()  # (r + c)
+    negDiag = set()  # (r - c)
+    
+    res = []
+    board = [["."] * n for _ in range(n)]
+    
+    def backtrack(r):
+        if r == n:
+            copy = ["".join(row) for row in board]
+            res.append(copy)
+            return
         
-        res = []
-        board = [["."] * n for i in range(n)]
-        
-        def backtrack(r):
-            if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
-                return
+        for c in range(n):
+            if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                continue
             
-            for c in range(n):
-                if c in col or (r + c) in posDiag or (r - c) in negDiag:
-                    continue
-                
-                col.add(c)
-                posDiag.add(r + c)
-                negDiag.add(r - c)
-                board[r][c] = "Q"
-                
-                backtrack(r + 1)
-                
-                col.remove(c)
-                posDiag.remove(r + c)
-                negDiag.remove(r - c)
-                board[r][c] = "."
-        
-        backtrack(0)
-        return res
+            col.add(c)
+            posDiag.add(r + c)
+            negDiag.add(r - c)
+            board[r][c] = "Q"
+            
+            backtrack(r + 1)
+            
+            col.remove(c)
+            posDiag.remove(r + c)
+            negDiag.remove(r - c)
+            board[r][c] = "."
+    
+    backtrack(0)
+    return res
 
 def main():
     if len(sys.argv) != 2:
@@ -51,8 +54,7 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
 
-    solver = Solution()
-    solutions = solver.solveNQueens(N)
+    solutions = solve_nqueens(N)
     for solution in solutions:
         print(solution)
 
