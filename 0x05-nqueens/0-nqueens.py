e935d2f7d26a006279e0b3c1143196c69770f58f
hyperayoub <bouagnaayoub@gmail.com>
2024-07-04 09:29:52 -0700

    n-queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -10,34 +10,34 @@ def solve_nqueens(n):
     posDiag = set()  # (r + c)
     negDiag = set()  # (r - c)
 
-    
     res = []
     board = [["."] * n for _ in range(n)]
-    
+
     def backtrack(r):
         if r == n:
             copy = ["".join(row) for row in board]
             res.append(copy)
             return
-        
+
         for c in range(n):
             if c in col or (r + c) in posDiag or (r - c) in negDiag:
                 continue
-            
+
             col.add(c)
             posDiag.add(r + c)
             negDiag.add(r - c)
             board[r][c] = "Q"
-            
+
             backtrack(r + 1)
-            
+
             col.remove(c)
             posDiag.remove(r + c)
             negDiag.remove(r - c)
             board[r][c] = "."
-    
+
     backtrack(0)
     return res
+
 
 def main():
     if len(sys.argv) != 2:
@@ -61,6 +61,7 @@ def main():
                 if solution[r][c] == "Q":
                     formatted_solution.append([r, c])
         print(formatted_solution)
+
 
 if __name__ == "__main__":
     main()
