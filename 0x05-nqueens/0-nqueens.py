489722ce4eb53b9638a7fb4f6eb2336d07c43e66
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-07-04 15:06:44 +0100

    Update 0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+import sys
+from typing import List
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posDiag = set()  # (r + c)
+        negDiag = set()  # (r - c)
+        
+        res = []
+        board = [["."] * n for i in range(n)]
+        
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+                
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+                
+                backtrack(r + 1)
+                
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+        
+        backtrack(0)
+        return res
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solver = Solution()
+    solutions = solver.solveNQueens(N)
+    for solution in solutions:
+        print(solution)
+
+if __name__ == "__main__":
+    main()
