fd8fb1bf538563fa95cf2af88a12425579914ebe
hyperayoub <bouagnaayoub@gmail.com>
2024-07-09 12:00:00 -0700

    rotate_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,7 +7,7 @@
 def rotate_2d_matrix(matrix):
     # length of matrix
     n = len(matrix)
-    # matrix here loop
+    # matrix[i][j] loop
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
