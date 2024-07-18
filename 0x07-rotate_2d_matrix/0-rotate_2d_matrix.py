15260d4f9fc6f64e46ffb26bbf60aba2630d6ff9
hyperayoub <bouagnaayoub@gmail.com>
2024-07-18 15:15:58 -0700

    reverse matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""n x n 2D matrix, rotate it 90 degrees clockwise """
+
+
+# function rotate matrix
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # reverse the rows
+    for row in matrix:
+        row.reverse()
