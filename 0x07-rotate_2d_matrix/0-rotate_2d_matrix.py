1cdea7b480b1d1595aaef7346332ff6e6dfebbd1
hyperayoub <bouagnaayoub@gmail.com>
2024-07-18 15:18:37 -0700

    reverse matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,7 +6,7 @@
 # function rotate matrix
 def rotate_2d_matrix(matrix):
     n = len(matrix)
-    # loop for rows && colomuns 
+    # loop for rows && colomuns
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
