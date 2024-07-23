4ede2a2d0f2e214a64033d07bf8d45dbfb67c40b
hyperayoub <bouagnaayoub@gmail.com>
2024-07-23 16:06:43 -0700

    coin change problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" coins problem """
+
+
+def makeChange(coins, total):
+    # If total is zero, no coins are needed
+    if total == 0:
+        return 0
+    # using bottom up
+
+    but = [total + 1] * (total + 1)
+    # start with fisrt posiition
+    but[0] = 0
+
+    for i in range(1, total + 1):
+        for j in coins:
+            if j <= i:
+                but[i] = min(but[i], 1 + but[i - j])
+
+    # retunn statement
+    return but[total] if but[total] != total + 1 else -1
