e5e0016d3bb652e37f8f4af5efd21468499ed371
hyperayoub <bouagnaayoub@gmail.com>
2024-08-13 13:34:18 -0700

    fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,7 +10,3 @@ def rotate_2d_matrix(matrix):
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-    # reverse the rows
-    for row in matrix:
-        row.reverse()
