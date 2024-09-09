902d486d36285889ee4f663a0ad0899bec8c481e
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-09-09 22:43:47 +0100

    rotate_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,8 +4,9 @@
 
 # Function to rotate the matrix
 def rotate_2d_matrix(matrix):
+    # length of matrix
     n = len(matrix)
-    # Step 1: Transpose the matrix by swapping rows with columns
+    # matrix here
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
